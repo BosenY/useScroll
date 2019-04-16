d9bf567f75fb0d30b794e1252703f5adeedb7832
boseny <yxy43445@gmail.com>
2019-04-16 21:00:24 +0800

    init

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+interface ScrollState {
+    x: number;
+    y: number;
+}
+export declare function useScroll(): ScrollState;
+export declare function useScrollX(): number;
+export declare function useScrollY(): number;
+export {};
